d7a86407905448f0f886604407d13f2bf5b9dbe9
Sean Alphonse <sean@aleksandr.cc>
2025-05-09 21:15:30 -0500

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ random-books.py
@@ -0,0 +1,15 @@
+import random
+
+if __name__ == '__main__':
+    books = ["Atomic Design by Brad Frost",
+             "Design of Everyday Things by Don Norman ",
+             "Continuous Discovery Habits: Discover Products that Create Customer Value and Business Value by Teresa Torres",
+             "Zero Stars, Do Not Recommend: A Novel by MJ Wassmer",
+             "The Midnight Library by Matt Haig",
+             "Trust: Living Spontaneously and Embracing Life by Osho",
+             "Ancient Christianities: The First Five Hundred Years by Paula Fredriksen",
+             "The Light Eaters: How the Unseen World of Plant Intelligence Offers a New Understanding of Life on Earth by Zoë Schlanger",
+             "The Gene: An Intimate History Siddhartha Mukherjee",
+             "What If?: Serious Scientific Answers to Absurd Hypothetical Questions Randall Munroe (looks like fun)",
+             "James by Percival Everett"]
+    print(random.choice(books))
